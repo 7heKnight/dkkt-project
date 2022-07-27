27f41daa57820b974706332baa654aca3a558c92
Ly Tuan Kiet <60308445+7heKnight@users.noreply.github.com>
2022-07-27 18:22:50 +0700

    Update dns_scan.py

--- dns_scan.py
+++ dns_scan.py
@@ -7,8 +7,7 @@ import re
 
 # Global variable
 session = requests.session()
-session.headers[
-    'User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
 banner = r'''==========================================================
      _ _    _    _                        _           _
   __| | | _| | _| |_      _ __  _ __ ___ (_) ___  ___| |_
